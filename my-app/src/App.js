c530105ae3b93b6dcc2d5b56894a6b9de1bf3092
Derelle Kirksey <derelle.p.kirksey@gmail.com>
2018-06-12 23:25:58 -0700

    refactoring components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
-import StarRatings from 'react-star-ratings';
+// import StarRatings from 'react-star-ratings';
 
 
 class App extends Component {
@@ -9,12 +9,25 @@ class App extends Component {
 
   constructor(props){
     super(props);
-      this.state = {};
+      this.state = {cityID:this.props.cityCode};
+      this.updateState = this.updateState.bind(this);
   }
 
+  updateState(){
+    this.setState(this.state);
+    console.log(this.state);
+  }
+
+  componentWillReceiveProps(){
+    this.setState({cityID:this.props.cityID}, function(){
+    console.log(this.state);
+  });
+}
+
+
   componentDidMount() { //stop updating with componentDidUpdate
-    let config = {'Authorization': 'Bearer 167873bcd96621d0bb49c45bfc0ffdc1', 'X-Zomato-API-Key': '167873bcd96621d0bb49c45bfc0ffdc1'};
-    axios.get('https://developers.zomato.com/api/v2.1/search?entity_id=302&entity_type=city&count=100', {headers: config}).then(res => {
+    let config = {'X-Zomato-API-Key': '167873bcd96621d0bb49c45bfc0ffdc1', 'entity_id':this.props.cityCode, 'entity_type':'city'};
+    axios.get('https://developers.zomato.com/api/v2.1/search', {headers: config}).then(res => {
       console.log(res.data.restaurants);
         let index = Math.floor(Math.random()*(res.data.restaurants.length)); //generates a random index
         this.setState({name: res.data.restaurants[index].restaurant.name,
@@ -23,7 +36,8 @@ class App extends Component {
                       rating: res.data.restaurants[index].restaurant.user_rating.aggregate_rating,
                       image: res.data.restaurants[index].restaurant.featured_image
                       }); //returned result must be converted to string since Object returns are not valid
-    })
+    });
+
   }
 
 
@@ -35,9 +49,11 @@ class App extends Component {
       <h2>{this.state.address}</h2>
       <h3>price: {this.state.price}</h3>
       <h3>rating: {this.state.rating}</h3>
-      <img src= {this.state.image} alt="cannot display image" />
+      <img src= {this.state.image} />
       </div>
     );
+    this.updateState();
+    console.log(this.state);
   };
 };
 
--- my-app/src/Forms.js
+++ my-app/src/Forms.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import App from './App.js';
 
 class Form extends Component{
   constructor(props){
@@ -10,28 +12,45 @@ class Form extends Component{
 
   }
 
+
   render(){
     return (
       <div><form>
       <input type="text" id="form" placeholder="Enter City (ex: Albany, NY)"></input>
       <button type="button" onClick={this.submitValue}>Submit</button>
       </form>
+      <App cityCode={this.state.cityID} changestate={this.changeState.bind(this)} />
       </div>
     )
 
   }
 
+  changeState(){
+    this.setState({town:this.state.cityID});
+  }
+
   getCityID(){
-    let config = {'Authorization': 'Bearer 167873bcd96621d0bb49c45bfc0ffdc1', 'X-Zomato-API-Key': '167873bcd96621d0bb49c45bfc0ffdc1'}
-    axios.get('https://developers.zomato.com/api/v2.1/locations?query=San%20Diego%2C%20CA', {headers:config}).then(res => {
-      console.log(res.location_suggestions.entity_id);
-      var cityID = res.location_suggestions.entity_id;
+    let query = this.state.cityName.replace(' ','%20').replace(', ','%2C%20');
+    console.log(query);
+     //the city needs to be a serialized code
+
+
+    let config = {'user-key': '167873bcd96621d0bb49c45bfc0ffdc1'}
+    console.log(config);
+    axios.get(`https://developers.zomato.com/api/v2.1/locations?query=${query}`, {headers:config}).then(res => {
+      console.log(res.data.location_suggestions[0].entity_id);
+      this.setState({cityID: res.data.location_suggestions[0].entity_id}, function(){
+        console.log(this.state);
+      });
     }
     )
   }
 
   submitValue(){
-      console.log(document.getElementById('form').value)
+      console.log(document.getElementById('form').value);
+      this.setState({cityName: document.getElementById('form').value}, function(){
+          this.getCityID();
+        });
   }
 }
 
