f6d03f6c44fefe1f491da782782aefc902e8d5dc
Derelle Kirksey <derelle.p.kirksey@gmail.com>
2018-06-13 23:55:04 -0700

    showInfo method added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,28 +10,19 @@ class App extends Component {
   constructor(props){
     super(props);
       this.state = {}
+      this.showInfo = this.showInfo.bind(this);
   }
 
 
   componentWillReceiveProps(nextProps){
     if (nextProps.cityID !== this.props.cityID){
     this.setState(nextProps, function(){
-      let entity_id = this.state.cityID;
-      console.log(this.state.cityID);
-      let config = {'user-key': '167873bcd96621d0bb49c45bfc0ffdc1'};
-      axios.get(`https://developers.zomato.com/api/v2.1/search?entity_id=${entity_id}&entity_type=city&count=40`, {headers: config}).then(res => {
-        console.log(res.data.restaurants);
-          let index = Math.floor(Math.random()*(res.data.restaurants.length)); //generates a random index
-          this.setState({name: res.data.restaurants[index].restaurant.name,
-                        address:res.data.restaurants[index].restaurant.location.address,
-                        price: res.data.restaurants[index].restaurant.price_range,
-                        rating: res.data.restaurants[index].restaurant.user_rating.aggregate_rating,
-                        image: res.data.restaurants[index].restaurant.featured_image
-                        }); //returned result must be converted to string since Object returns are not valid
-      });
+      this.showInfo();
     });
   }
 }
+
+
 
   componentDidMount(){
     console.log(this.state);
@@ -39,18 +30,23 @@ class App extends Component {
 
   componentDidUpdate() {
     console.log(this.state);
-    // let cityString = this.state.cityID.toString();
-    // let config = {'X-Zomato-API-Key': '167873bcd96621d0bb49c45bfc0ffdc1', 'entity_id': cityString, 'entity_type':'city'};
-    // axios.get('https://developers.zomato.com/api/v2.1/search', {headers: config}).then(res => {
-    //   console.log(res.data.restaurants);
-    //     let index = Math.floor(Math.random()*(res.data.restaurants.length)); //generates a random index
-    //     this.setState({name: res.data.restaurants[index].restaurant.name,
-    //                   address:res.data.restaurants[index].restaurant.location.address,
-    //                   price: res.data.restaurants[index].restaurant.price_range,
-    //                   rating: res.data.restaurants[index].restaurant.user_rating.aggregate_rating,
-    //                   image: res.data.restaurants[index].restaurant.featured_image
-    //                   }); //returned result must be converted to string since Object returns are not valid
-    // });
+  }
+
+  showInfo() {
+    console.log(this.state.cityID);
+    let entity_id = this.state.cityID;
+    let config = {'user-key': '167873bcd96621d0bb49c45bfc0ffdc1'};
+    axios.get(`https://developers.zomato.com/api/v2.1/search?entity_id=${entity_id}&entity_type=city&count=40`, {headers: config}).then(res => {
+      console.log(res.data.restaurants);
+        var index = Math.floor(Math.random()*(res.data.restaurants.length)); //generates a random index
+        console.log(index);
+        this.setState({name: res.data.restaurants[index].restaurant.name,
+                      address:res.data.restaurants[index].restaurant.location.address,
+                      price: res.data.restaurants[index].restaurant.price_range,
+                      rating: res.data.restaurants[index].restaurant.user_rating.aggregate_rating,
+                      image: res.data.restaurants[index].restaurant.featured_image
+                      }); //returned result must be converted to string since Object returns are not valid
+    });
   }
 
 
@@ -63,7 +59,8 @@ class App extends Component {
       <h2>{this.state.address}</h2>
       <h3>price: {this.state.price}</h3>
       <h3>rating: {this.state.rating}</h3>
-      <img src= {this.state.image} alt="" />
+      <img src= {this.state.image} alt="no image" />
+      <button onClick={this.showInfo}>Show me another!</button>
       </div>
     );
   };
