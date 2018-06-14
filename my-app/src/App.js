ef00678c33c443d359bb111d9adfe1b5be05b585
Derelle Kirksey <derelle.p.kirksey@gmail.com>
2018-06-13 23:33:36 -0700

    converted functionality of lifecycle methods

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,37 +9,48 @@ class App extends Component {
 
   constructor(props){
     super(props);
-      this.state = {cityID:this.props.cityID};
+      this.state = {}
   }
 
 
   componentWillReceiveProps(nextProps){
-    console.log(nextProps);
-    this.setState(nextProps);
+    if (nextProps.cityID !== this.props.cityID){
+    this.setState(nextProps, function(){
+      let entity_id = this.state.cityID;
+      console.log(this.state.cityID);
+      let config = {'user-key': '167873bcd96621d0bb49c45bfc0ffdc1'};
+      axios.get(`https://developers.zomato.com/api/v2.1/search?entity_id=${entity_id}&entity_type=city&count=40`, {headers: config}).then(res => {
+        console.log(res.data.restaurants);
+          let index = Math.floor(Math.random()*(res.data.restaurants.length)); //generates a random index
+          this.setState({name: res.data.restaurants[index].restaurant.name,
+                        address:res.data.restaurants[index].restaurant.location.address,
+                        price: res.data.restaurants[index].restaurant.price_range,
+                        rating: res.data.restaurants[index].restaurant.user_rating.aggregate_rating,
+                        image: res.data.restaurants[index].restaurant.featured_image
+                        }); //returned result must be converted to string since Object returns are not valid
+      });
+    });
+  }
+}
+
+  componentDidMount(){
     console.log(this.state);
   }
 
-  componentDidUpdate(prevState){
-    if (prevState === this.state){
-      this.setState({cityID: this.props.cityID})
-      console.log(this.state);
-    }
-
-  }
-
-
-  componentDidMount() {
-    let config = {'X-Zomato-API-Key': '167873bcd96621d0bb49c45bfc0ffdc1', 'entity_id':this.state.cityCode, 'entity_type':'city'};
-    axios.get('https://developers.zomato.com/api/v2.1/search', {headers: config}).then(res => {
-      console.log(res.data.restaurants);
-        let index = Math.floor(Math.random()*(res.data.restaurants.length)); //generates a random index
-        this.setState({name: res.data.restaurants[index].restaurant.name,
-                      address:res.data.restaurants[index].restaurant.location.address,
-                      price: res.data.restaurants[index].restaurant.price_range,
-                      rating: res.data.restaurants[index].restaurant.user_rating.aggregate_rating,
-                      image: res.data.restaurants[index].restaurant.featured_image
-                      }); //returned result must be converted to string since Object returns are not valid
-    });
+  componentDidUpdate() {
+    console.log(this.state);
+    // let cityString = this.state.cityID.toString();
+    // let config = {'X-Zomato-API-Key': '167873bcd96621d0bb49c45bfc0ffdc1', 'entity_id': cityString, 'entity_type':'city'};
+    // axios.get('https://developers.zomato.com/api/v2.1/search', {headers: config}).then(res => {
+    //   console.log(res.data.restaurants);
+    //     let index = Math.floor(Math.random()*(res.data.restaurants.length)); //generates a random index
+    //     this.setState({name: res.data.restaurants[index].restaurant.name,
+    //                   address:res.data.restaurants[index].restaurant.location.address,
+    //                   price: res.data.restaurants[index].restaurant.price_range,
+    //                   rating: res.data.restaurants[index].restaurant.user_rating.aggregate_rating,
+    //                   image: res.data.restaurants[index].restaurant.featured_image
+    //                   }); //returned result must be converted to string since Object returns are not valid
+    // });
   }
 
 
